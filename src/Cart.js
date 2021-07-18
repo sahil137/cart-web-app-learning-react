fd6afe763a2f212adf7442ef69f3bf09696dd833
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-07-18 16:50:11 +0530

    add props to cart item

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,46 @@ import React from 'react';
 import CartItem from './CartItem';
 
 class Cart extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            products: [
+                {
+                    price: 999,
+                    title: 'Mobile Phone',
+                    qty: 1,
+                    img: '',
+                    id : 1
+                },
+                {
+                    price: 99,
+                    title: 'Watch',
+                    qty: 2,
+                    img: '',
+                    id : 2
+                },
+                {
+                    price: 9999,
+                    title: 'Laptop',
+                    qty: 1,
+                    img: '',
+                    id : 3
+                }
+            ]
+        }
+    }
     render() {
+        const {products} = this.state;
         return (
             <div className = "cart">
-                <CartItem />
-                <CartItem />
-                <CartItem />
+                {products.map((product) => {
+                    return (
+                        <CartItem 
+                            product = {product} 
+                            key = {product.id}
+                        />
+                    )
+                })}
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,9 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            price: 999,
-            title: 'Phone',
-            qty: 1,
-            img: ''
-        }
-    }
     increaseQuantity = () => {
         // this.state.qty += 1;
-        console.log('this', this.state);
+        // console.log('this', this.state);
         // setState form 1
         // this.setState({
         //     qty: this.state.qty + 1
@@ -37,15 +28,15 @@ class CartItem extends React.Component {
         })
     }
     render(){
-        const  {price, title, qty} = this.state;
+        const  {price, title, qty} = this.props.product;
         return (
             <div className = "cart-item">
                 <div className = "left-block">
                     <img style = {styles.image} alt = ""/>
                 </div>
                 <div className = "right-block">
-                    <div style = { {fontSize: 25} }>{price}</div>
-                    <div style = { {color: '#777'} }>{title}</div>
+                    <div style = { {fontSize: 25} }>{title}</div>
+                    <div style = { {color: '#777'} }>{price}</div>
                     <div style = { {color: '#777'} }>{qty}</div>
                     <div className = "cart-item-actions">
                         {/* Buttons */}

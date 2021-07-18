359fa2ef38c5b77ef339fa9a216b8c2b8e5ef443
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-07-18 20:14:27 +0530

    add total price

--- src/App.js
+++ src/App.js
@@ -79,6 +79,17 @@ class App extends React.Component {
     return count;
   }
 
+  getCartTotal = () => {
+    const  {products} = this.state;
+
+    let cartTotal = 0;
+
+    products.map((product) => {
+      cartTotal = cartTotal + product.qty * product.price
+    })
+    return cartTotal;
+  }
+
   render() {
     const {products} = this.state;
     return (
@@ -92,6 +103,7 @@ class App extends React.Component {
           onDecreaseQuantity = {this.handleDecreaseQuantity}
           onDeleteProduct = {this.handleDeleteProduct}
         />
+        <div>TOTAL: {this.getCartTotal()}</div>
       </div>
     );
   }
